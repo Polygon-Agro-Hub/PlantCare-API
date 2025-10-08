2ec443566fad8282f6770bc565d92c03bcdfcbb1
Hahinika Dilrukshi <ghashinika2002@gmail.com>
2025-10-08 18:05:30 +0530

    bug fix

--- dao/news-dao.js
+++ dao/news-dao.js
@@ -1,9 +1,27 @@
 const db = require('../startup/database');
 
 
+// exports.getAllNewsData = () => {
+//   return new Promise((resolve, reject) => {
+//     const sql = 'SELECT * FROM content WHERE status = "Published"';
+//     db.plantcare.query(sql, (err, results) => {
+//       if (err) {
+//         reject('Error executing query: ' + err);
+//       } else {
+//         resolve(results);
+//       }
+//     });
+//   });
+// };
+
 exports.getAllNewsData = () => {
   return new Promise((resolve, reject) => {
-    const sql = 'SELECT * FROM content WHERE status = "Published"';
+    const sql = `
+      SELECT * FROM content 
+      WHERE status = "Published" 
+      AND publishDate <= NOW() 
+      AND expireDate >= NOW()
+    `;
     db.plantcare.query(sql, (err, results) => {
       if (err) {
         reject('Error executing query: ' + err);
